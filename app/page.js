5f95984b59e67888d3e3fa816807476231c8353c
Kevin Hui <kevinhui98@gmail.com>
2024-08-01 15:09:07 -0400

    adding catagories and quantity for dynamic add

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 'use client'
-import { Box, Stack, Typography, Button, Modal, TextField } from "@mui/material";
+import { Box, Stack, Typography, Button, Modal, TextField, IconButton, MenuItem, FormControl, Select, InputLabel } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
 import { firestore } from "../firebase";
 import { collection, doc, getDocs, query, setDoc, deleteDoc, getDoc, where } from "firebase/firestore";
 import { useEffect, useState } from "react";
@@ -26,14 +27,20 @@ export default function Home() {
   const [addOpen, setAddOpen] = useState(false);
   const handleAddOpen = () => setAddOpen(true);
   const handleAddClose = () => setAddOpen(false);
-
+  // Search Modal state
   const [searchOpen, setSearchOpen] = useState(false);
   const handleSearchOpen = () => setSearchOpen(true);
   const handleSearchClose = () => setSearchOpen(false);
-
+  // Quantity state
+  const [quantity, setQuantity] = useState(1);
+  const [quantityOpen, setQuantityOpen] = useState(false);
+  const handleQuantityChange = (event) => setQuantity(event.target.value);
+  const handleQuantityClose = () => setQuantityOpen(false);
+  const handleQuantityOpen = () => setQuantityOpen(true);
 
   // Searching for items within modal
   const [itemname, setItemName] = useState('');
+  const [catagory, setCatagory] = useState('');
   const [searchItem, setSearchItem] = useState('');
   const [itemCount, setItemCount] = useState(0);
   // useEffect to get all the pantry items from firestore
@@ -50,19 +57,20 @@ export default function Home() {
     setPantry(pantryList);
   }
 
-  const addItem = async (item) => {
-    const docRef = doc(collection(firestore, 'pantry'), item)
+  const addItem = async (cat, item, quantity) => {
+    if (cat === "") cat = "pantry";
+    const docRef = doc(collection(firestore, cat), item)
     const subCat = await getDoc(docRef);
     //if the document already exists, update the count
     if (subCat.exists()) {
       // console.log("subCat ", subCat.data());
       const name = subCat.data().name;
-      const count = subCat.data().count + 1;
-      await setDoc(docRef, { name, count });
+      const count = subCat.data().count + quantity;
+      await setDoc(docRef, { name, count, catagory: cat });
     }
     else {
       //if the document does not exist, create a new document with the item name and count 1
-      await setDoc(docRef, { name: item, count: 1 });
+      await setDoc(docRef, { name: item, count: quantity, catagory: cat });
 
     }
     await updatePantries();
@@ -123,17 +131,49 @@ export default function Home() {
             Add Item
           </Typography>
           <Stack direction={"column"} spacing={2}>
-            <TextField id="outlined-basic" label="item" variant="outlined" fullWidth
-              onChange={(e) => setItemName(e.target.value)}
+            <TextField id="outlined-basic" label="pantry (optional)" variant="outlined" fullWidth
+              onChange={(e) => setCatagory(e.target.value)}
             />
+            <Stack direction={'row'} spacing={2}>
+              <TextField id="outlined-basic" label="item" variant="outlined" fullWidth
+                onChange={(e) => setItemName(e.target.value)} required
+              />
+              <FormControl sx={{ m: 1, minWidth: 120 }} >
+                <InputLabel id="demo-controlled-open-select-label">Quantity</InputLabel>
+                <Select
+                  labelId="demo-controlled-open-select-label"
+                  id="demo-controlled-open-select"
+                  open={quantityOpen}
+                  onClose={handleQuantityClose}
+                  onOpen={handleQuantityOpen}
+                  label="Quantity"
+                  value={quantity}
+                  onChange={(e) => {
+                    setQuantity(e.target.value)
+                    handleQuantityClose
+                  }}
+                >
+                  <MenuItem value={1}>1</MenuItem>
+                  <MenuItem value={2}>2</MenuItem>
+                  <MenuItem value={3}>3</MenuItem>
+                  <MenuItem value={4}>4</MenuItem>
+                  <MenuItem value={5}>5</MenuItem>
+                  <MenuItem value={6}>6</MenuItem>
+                  <MenuItem value={7}>7</MenuItem>
+                  <MenuItem value={8}>8</MenuItem>
+                  <MenuItem value={9}>9</MenuItem>
+                </Select>
+              </FormControl>
+            </Stack>
             <Button variant={"contained"} color={"primary"}
               onClick={() => {
-                addItem(itemname)
+                console.log(catagory, itemname)
+                addItem(catagory, itemname, quantity)
+                setCatagory("")
                 setItemName("")
+                setQuantity(0)
                 handleAddClose()
-              }
-
-              }>Add</Button>
+              }}>Add</Button>
           </Stack>
         </Box>
       </Modal>
@@ -182,7 +222,7 @@ export default function Home() {
         </Box>
         <Stack
           width={'800px'}
-          height={'300px'}
+          height={'600px'}
           spacing={2}
           overflow={'scroll'}
         >
@@ -212,7 +252,9 @@ export default function Home() {
                   color={"#333"}
                   textAlign={'center'}>
                   Quantity: {count}</Typography>
-                <Button variant={"contained"} color={"error"} onClick={() => removeItem(name)} size="small" >Remove</Button>
+                <IconButton aria-label="delete" color={"error"} onClick={() => removeItem(name)}>
+                  <DeleteIcon />
+                </IconButton>
               </Box>
             ))
           }
