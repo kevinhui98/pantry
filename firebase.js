906684a3d03904b7929d8db800f591f84a58b297
Kevin Hui <kevinhui98@gmail.com>
2024-08-01 01:40:56 -0400

    remove keys

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDmBP4kVbbjM4q_Yec5s9NFd-T-7O1yvso",
-    authDomain: "hspantryapp-9188a.firebaseapp.com",
-    projectId: "hspantryapp-9188a",
-    storageBucket: "hspantryapp-9188a.appspot.com",
-    messagingSenderId: "159465706602",
-    appId: "1:159465706602:web:77af1d25f83b897b432613"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
